93ed1d1cf96e756951db2ebb411bb1736a7b10cf
root <anthony.n.ndegwa@gmail.com>
2023-08-10 00:31:03 -0700

    Add method def session_cookie

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -32,3 +32,9 @@ class Auth:
     def current_user(self, request=None) -> TypeVar('User'):
         """Get the current user from the request."""
         return None
+
+    def session_cookie(self, request=None) -> str:
+        """Returns a cookie value from a request."""
+        if request is not None:
+            cookie_name = os.getenv('SESSION_NAME')
+            return request.cookies.get(cookie_name)
