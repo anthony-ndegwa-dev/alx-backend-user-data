1547f028449a3e4b881f61ebb6663764b3138517
root <anthony.n.ndegwa@gmail.com>
2023-08-15 06:51:24 -0700

    A module to hash password

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""A module to hash password
+"""
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """Hashes a password.
+    """
+    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
