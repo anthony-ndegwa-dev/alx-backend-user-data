37c94fc3187fc5494f66a118a51aa39cda146e0c
root <anthony.n.ndegwa@gmail.com>
2023-08-16 23:38:23 -0700

    Modify code"

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -3,14 +3,14 @@
 """
 
 
-def register_user(email: str, password: str) -> None
-def log_in_wrong_password(email: str, password: str) -> None
-def log_in(email: str, password: str) -> str
-def profile_unlogged() -> None
-def profile_logged(session_id: str) -> None
-def log_out(session_id: str) -> None
-def reset_password_token(email: str) -> str
-def update_password(email: str, reset_token: str, new_password: str) -> None
+def register_user(email: str, password: str) -> None:
+def log_in_wrong_password(email: str, password: str) -> None:
+def log_in(email: str, password: str) -> str:
+def profile_unlogged() -> None:
+def profile_logged(session_id: str) -> None:
+def log_out(session_id: str) -> None:
+def reset_password_token(email: str) -> str:
+def update_password(email: str, reset_token: str, new_password: str) -> None:
 
 
 EMAIL = "guillaume@holberton.io"
