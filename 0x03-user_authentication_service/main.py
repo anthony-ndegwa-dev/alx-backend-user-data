e6d60233084c2d612d76a4201ad46f8aa6b48a0a
root <anthony.n.ndegwa@gmail.com>
2023-08-16 23:20:10 -0700

    Test profile information retrieval while logged in

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -49,6 +49,16 @@ def profile_unlogged() -> None
 
 
 def profile_logged(session_id: str) -> None
+    """Test profile information retrieval while logged in."""
+    url = "{}/profile".format(BASE_URL)
+    req_cookies = {
+        'session_id': session_id,
+    }
+    res = requests.get(url, cookies=req_cookies)
+    assert res.status_code == 200
+    assert "email" in res.json()
+
+
 def log_out(session_id: str) -> None
 def reset_password_token(email: str) -> str
 def update_password(email: str, reset_token: str, new_password: str) -> None
