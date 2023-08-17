aaad73f79ce4e5bcd051fa773b4b8b3c576161f4
root <anthony.n.ndegwa@gmail.com>
2023-08-16 23:57:06 -0700

    End-to-end integration test

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """End-to-end integration test for app.py
 """
+import requests
 
 
 def register_user(email: str, password: str) -> None:
@@ -99,6 +100,7 @@ def update_password(email: str, reset_token: str, new_password: str) -> None:
 EMAIL = "guillaume@holberton.io"
 PASSWD = "b4l0u"
 NEW_PASSWD = "t4rt1fl3tt3"
+BASE_URL = "http://0.0.0.0:5000"
 
 
 if __name__ == "__main__":
